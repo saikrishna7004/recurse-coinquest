b83d885304c295f6587a602437c4d382a8ffe39c
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2024-03-02 13:29:15 +0530

    Fixed hunt broken button

--- pages/hunt.js
+++ pages/hunt.js
@@ -36,11 +36,15 @@ const TreasureHunt = () => {
                 <p className="my-3">Check out for the hints posted in the below link frequently for finding the hints of treasure. </p>
                 <p className="mt-3 mb-4">Find the QR and upload it in the space provided here.</p>
             </div>
-            <div className="my-4 p-2">
+            <div className="my-4 p-2 ms-3">
                 <Link className='button my-3 me-3' href='https://drive.google.com/drive/folders/1xRBtZd5NR4FkVM2xVDLNL95i1i8e-01F?usp=sharing' target="_blank">Check for Hints</Link>
-                <Link className='button my-3 me-3' href='/code'>Enter the code</Link>
-                {session && session.user && session.user.admin && <Link className='button my-3' href='/admin/hunt'>Edit Hints</Link>}
             </div>
+            <div className="my-4 p-2 ms-3">
+                <Link className='button my-3 me-3' href='/code'>Enter the code</Link><br />
+            </div>
+            {session && session.user && session.user.admin && <div className="my-4 p-2 text-center">
+                <Link className='button my-3' href='/admin/hunt'>Edit Hints</Link>
+            </div>}
             <div className="container my-4">
                 <h4 className="my-4">Solved Treasures</h4>
                 <div className="table-responsive my-4 ">
